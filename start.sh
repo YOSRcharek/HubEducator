0147263c18642cdbf18f83cb38ac0a7257736a50
YOSRcharek <yosryassoura31@gmail.com>
2025-10-01 00:08:17 +0100

    start

--- start.sh
+++ start.sh
@@ -3,11 +3,14 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Apply database migrations
+# Make sure Gunicorn is installed
+pip install gunicorn
+
+# Apply migrations
 python manage.py migrate --noinput
 
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Start the Django app with Gunicorn
+# Start the app
 gunicorn HubEducator.wsgi:application --bind 0.0.0.0:$PORT
