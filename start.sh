1bd81477a90878fd99471db25335d4e4bd049496
YOSRcharek <yosryassoura31@gmail.com>
2025-10-01 00:16:34 +0100

    start

--- start.sh
+++ start.sh
@@ -3,14 +3,11 @@
 # Install dependencies
 pip install -r requirements.txt
 
-# Make sure Gunicorn is installed
-pip install gunicorn
-
 # Apply migrations
 python manage.py migrate --noinput
 
 # Collect static files
 python manage.py collectstatic --noinput
 
-# Start the app
-gunicorn HubEducator.wsgi:application --bind 0.0.0.0:$PORT
+# Start the Django app on the port Railway provides
+python manage.py runserver 0.0.0.0:$PORT
