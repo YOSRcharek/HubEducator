473a33c11f12929cf5d40e324de52e63911ad5fb
YOSRcharek <yosryassoura31@gmail.com>
2025-10-03 04:54:31 +0100

    verification&forgetpass

--- HubEducator/settings.py
+++ HubEducator/settings.py
@@ -159,28 +159,16 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # must be different from ST
 # WhiteNoise for serving static files in production
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
 if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
-    # Production : vrai envoi via SMTP
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_HOST = os.getenv("EMAIL_HOST")
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
     EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
     EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
     EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "False") == "True"
     EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
     EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-    DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
-
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # or your SMTP server
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'classyy2023@gmail.com'
-EMAIL_HOST_PASSWORD = 'sqobpmxusgwjheys'
-DEFAULT_FROM_EMAIL = 'HubEducator HubEducator@gmail.com'
-
+    DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", EMAIL_HOST_USER)
