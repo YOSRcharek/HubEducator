b3bcf06e16e1d66883ab434be705ac678228bc7a
douabaghdadi <azizbelhadj51@gmail.com>
2025-10-22 19:09:39 +0100

    Transaction Subscribe + Unsbuscribe + UserSubscriptions + Payment History

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, Course, Chapter, Exercise, Subscription, Transaction, UserSubscription
+from .models import User, Course, Exercise, Subscription, Transaction, UserSubscription
 
 # Register your models here.
 
