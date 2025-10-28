c627a8c5c786c50e148f0b6835e9a06bd7902a33
Abdessalem Chaouch <abdessalem.chaouch@esprit.tn>
2025-10-28 18:37:05 +0100

    resolve conflit

--- website/urls.py
+++ website/urls.py
@@ -15,23 +15,17 @@ urlpatterns = [
     path('profil/', core_views.profil, name='profil'),
     path('pricing/', views.pricing, name='pricing'),
     path('web-development/', views.web_development, name='web-development'),
-<<<<<<< HEAD
     path('certificates/', views.certificates, name='certificates'),
     path('certificates/<int:cert_id>/', views.certificate_detail, name='certificate_detail'),
     path('certificates/take/<int:cert_id>/', views.take_certificate, name='take_certificate'),
     path('certificates/result/<int:attempt_id>/', views.certificate_result, name='certificate_result'),
     path('my-certificates/', views.my_certificates, name='my_certificates'),
-=======
->>>>>>> main
     path('course-details/', views.courseDetails, name='courseDetails'),
     path('user-research/', views.user_research, name='user-research'),
     path("verify-code/", views.verify_code_view, name="verify_code"),
     path('resend-code/', views.resend_code_view, name='resend_code'),
     path('auth/google/callback/', views.google_callback, name='google_callback'),
-<<<<<<< HEAD
     
-=======
->>>>>>> main
 
 
     #***********************************************************#
@@ -49,8 +43,6 @@ urlpatterns = [
     path('reset/done/', 
          auth_views.PasswordResetCompleteView.as_view(template_name='ResetPassword/password_reset_complete.html'), 
          name='password_reset_complete'),
-<<<<<<< HEAD
-=======
     
     #***********************************************************#
     #****************Payment System***********************#
@@ -59,6 +51,5 @@ urlpatterns = [
     path('payment/success/', views.payment_success, name='payment_success'),
     path('payment/failed/', views.payment_failed, name='payment_failed'),
     path('payment/webhook/', views.stripe_webhook, name='stripe_webhook'),
->>>>>>> main
 ]
 
